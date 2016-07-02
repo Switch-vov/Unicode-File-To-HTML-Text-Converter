4bab434dfe0c26f7d843c68666f994b9ac61263b
switch <q547550831@outlook.com>
2016-07-02 17:02:00 +0800

    Added 3 user intent test TODOs.

--- ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
+++ ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
@@ -11,4 +11,10 @@ public class UnicodeFileToHtmlTextConverterTest {
     }
 
     // TODO-new-feature: Make the UnicodeFileToHtmlTextConverter working for not only a file but also a string
+
+    // TODO-user-intent-test: should convert ampersand
+
+    // TODO-user-intent-test: should convert greater than and less than
+
+    // TODO-user-intent-test: should add a line break for a new line
 }
