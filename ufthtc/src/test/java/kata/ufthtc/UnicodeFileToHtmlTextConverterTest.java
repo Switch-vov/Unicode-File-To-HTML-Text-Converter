4c608afac1e4239e6e04e381525b6b24b0ea11e2
switch <q547550831@outlook.com>
2016-07-02 13:36:12 +0800

    Initial commit

---
+++ ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
@@ -0,0 +1,12 @@
+package kata.ufthtc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UnicodeFileToHtmlTextConverterTest {
+    @Test
+    public void a_trivial_test() {
+        assertEquals(5, 2 + 3);
+    }
+}
