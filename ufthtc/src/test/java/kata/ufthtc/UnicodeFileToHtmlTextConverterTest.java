34d6306ce878ad071430e18c9d8b95fa58f6f94c
switch <q547550831@outlook.com>
2016-07-02 17:30:02 +0800

    Finished TODO: should convert greater than and less than.

--- ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
+++ ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
@@ -15,17 +15,24 @@ public class UnicodeFileToHtmlTextConverterTest {
 
     // TODO-new-feature: Make the UnicodeFileToHtmlTextConverter working for not only a file but also a string
 
-    // TODO-user-intent-test-working-on: should convert ampersand
     @Test
     public void should_convert_ampersand() throws IOException {
         // Arrange
         UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(new StringReader("H&M"));
 
         // Act & Assert
-        assertEquals("", converter.convertToHtml());
+        assertEquals("H&amp;M<br />", converter.convertToHtml());
     }
 
-    // TODO-user-intent-test: should convert greater than and less than
+    @Test
+    public void should_convert_greater_than_and_less_than() throws IOException {
+        // Arrange
+        UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(new StringReader(">_<|||"));
+
+        // Act & Assert
+        assertEquals("&gt;_&lt;|||<br />", converter.convertToHtml());
+    }
+
 
     // TODO-user-intent-test: should add a line break for a new line
 }
