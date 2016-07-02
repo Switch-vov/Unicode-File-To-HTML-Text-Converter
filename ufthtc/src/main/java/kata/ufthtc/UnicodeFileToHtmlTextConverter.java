96dbec047ed47f1f2b173c2c93ba40526b78abb7
switch <q547550831@outlook.com>
2016-07-02 17:19:45 +0800

    Fixed null pointer exception: Implemented constructor UnicodeFileToHtmlTextConverter(Reader).

--- ufthtc/src/main/java/kata/ufthtc/UnicodeFileToHtmlTextConverter.java
+++ ufthtc/src/main/java/kata/ufthtc/UnicodeFileToHtmlTextConverter.java
@@ -14,7 +14,7 @@ public class UnicodeFileToHtmlTextConverter {
     }
 
     public UnicodeFileToHtmlTextConverter(Reader reader) {
-
+        this.reader = reader;
     }
 
     public String convertToHtml() throws IOException{
